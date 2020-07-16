1ec39b70ce37286455228fb5fd948446aa2e7ecf
samanthajmaas <samanthajmaas@gmail.comcdclear>
2020-07-16 16:05:14 -0500

    starting working on Harvester

---
+++ src/scripts/harvester.js
@@ -0,0 +1,10 @@
+import {usePlants} from "./field.js"
+let harvestedPlants = ""
+
+export const harvestPlants = (plantArray) => {
+for (const seed of plantArray) {
+    
+
+    }
+
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,8 @@
 import { createPlan } from "./plan.js"
 import {plantSeeds} from "./tractor.js"
+import {usePlants} from "./field.js"
 
+
+const arrayOfPlants = usePlants()
 const yearlyPlan = createPlan()
 
