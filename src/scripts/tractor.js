c554afba76d3623abdaccb7bdc5c4dfd85b984d0
samanthajmaas <samanthajmaas@gmail.comcdclear>
2020-07-16 15:37:32 -0500

    Completed tilling and sowing

--- src/scripts/tractor.js
+++ src/scripts/tractor.js
@@ -1,10 +1,34 @@
-const plantingPlan = [
-    ["Potato", "Soybean", "Soybean", "Corn"],
-    ["Wheat", "Corn", "Wheat", "Asparagus"],
-    ["Asparagus", "Wheat", "Soybean", "Corn"],
-    ["Asparagus", "Soybean", "Potato", "Wheat"]
-]
+import { createAsparagus } from "./seeds/asparagus"
+import {createCorn} from "./seeds/corn.js"
+import {createPotato} from "./seeds/potato.js"
+import {createSoybean} from "./seeds/soybean.js"
+import {createSunflower} from "./seeds/sunflower.js"
+import{createWheat} from "./seeds/wheat.js"
+import { addPlant } from "./field"
 
-export const plantSeeds = (plantingPlan) => {
-    for (const seed of )
+export const plantSeeds = (plantingArray) => {
+ for (const array of plantingArray) {
+    for (const seed of array){
+     if ( plantObj.type === "Asparagus") {
+         const asparagusSeed = createAsparagus()
+         addPlant(asparagusSeed)
+     } else if (plantObj.type === "Corn") {
+         const cornSeed= createCorn()
+         addPlant(cornSeed)
+     } else if (plantObj.type === "Potato") {
+         const potatoSeed= createPotato()
+         addPlant(potatoSeed)
+     } else if (plantObj.type === "Soybean") {
+         const soybeanSeed =createSoybean()
+         addPlant(soybeanSeed)
+     } else if (plantObj.type === "Sunflower") {
+         const sunflowerSeed = createSunflower()
+         addPlant(sunflowerSeed) 
+     } else if (plantObj.type === "Wheat") {
+         const wheatSeed =createWheat()
+         addPlant(wheatSeed)
+     }
+ }
 }
+}
+
