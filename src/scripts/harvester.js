b018771b9dcb4168336b7878435e072b92ffedf5
samanthajmaas <samanthajmaas@gmail.comcdclear>
2020-07-17 09:49:50 -0500

    Finished Harvester

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -1,10 +1,19 @@
 import {usePlants} from "./field.js"
-let harvestedPlants = ""
+
+let harvestedPlants = []
 
 export const harvestPlants = (plantArray) => {
 for (const seed of plantArray) {
-    
-
+   if (plantObj.type === "Corn") {
+    for (let j = 0; j < plantObj.output/2; j++) {
+        harvestedPlants.push(seed)
     }
 
+    } else {
+        for (let i = 0; i < plantObj.output; i++) {
+            harvestedPlants.push(seed)
+        }
+    }
+    return harvestedPlants
+}
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,11 @@
 import { createPlan } from "./plan.js"
 import {plantSeeds} from "./tractor.js"
 import {usePlants} from "./field.js"
+import {harvestPlants} from "./harvester.js"
 
-
-const arrayOfPlants = usePlants()
 const yearlyPlan = createPlan()
-
+console.log(yearlyPlan)
+const arrayOfPlants = usePlants()
+plantSeeds(arrayOfPlants)
+const allHarvestedPlants = harvestPlants(arrayOfPlants)
+console.log(allHarvestedPlants)
